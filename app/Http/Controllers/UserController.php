21f9478cfcb9784c5b7f83cfaae5a113a618304d
dewikartika2016 <73477002+dewikartika2016@users.noreply.github.com>
2021-12-01 14:35:44 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,13 +11,16 @@ class UserController extends Controller
     public function register(Request $request){
         $this->validate($request, [
             'email' => 'required|unique:users|email',
+            'username' => 'required',
             'password' => 'required|min:6'
         ]);
         $email = $request->input('email');
+        $username = $request->input('username');
         $password = Hash::make($request->input('password'));
 
         $user = User::create([
             'email' => $email,
+            'username' => $username,
             'password' => $password
         ]);
 
