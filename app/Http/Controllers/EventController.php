7e5571feac3fa50d796dac817cfdc4924352161c
auroraufa <73575715+auroraufa@users.noreply.github.com>
2021-12-18 12:32:31 +0700

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -15,10 +15,11 @@ class EventController extends Controller
 
     public function show($jenis)
     {
-        return response()->json([
+   
+        $eventList = new stdClass();
+         return response()->json([
             'message' => 'Masuk kesini kok'
         ]);
-        $eventList = new stdClass();
         $events = Event::where('jenis', $jenis) -> select ('jenis', 'kategori_id', 'nama_event', 'date')->get();
         $eventList-> event= $events;
         return response()->json($eventList);
