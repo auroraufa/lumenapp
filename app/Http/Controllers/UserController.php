d967d059e9570be78f495abeee2600f15f9651b5
auroraufa <ufaaurora@gmail.com>
2022-01-05 14:54:03 +0700

    test API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriUser;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -31,6 +33,27 @@ class UserController extends Controller
             'message' => 'Pendaftaran pengguna berhasil dilaksanakan',
             'token' => $user->token,
             'idUser' => $user->id
+        ]);
+    }
+
+    public function addKategrori(Request $request)
+    {
+        $auth = Auth::user();
+        $user = User::where('id', $auth->id)->first();
+
+        $this->validate($request, [
+            'user_id' => $user,
+            'kategori' => 'required'
+        ]);
+        $kategori = $request->input('kategori');
+
+        $kategori_user = KategoriUser::create([
+            'user_id' => $user,
+            'kategori' => $kategori
+        ]);
+
+        return response()->json([
+            'message' => 'Tema Favorite anda telah berhasil ditambah'
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -43,4 +43,5 @@ $router->group(['middleware' => 'auth'], function () use ($router) {
     $router->get('/api/seminar/{jenis}', 'EventController@show');
     $router->post('/logout', 'AuthController@logout');
     $router->get('/api/kategori', 'KategoriController@getkategori');
+    $router->post('/api/addKategori', 'UserController@addKategori');
 });
